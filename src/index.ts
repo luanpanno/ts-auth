371bdca1f39284f329c24fd052e2820e758748f5
Luan Panno <luanpanno@gmail.com>
2021-10-09 00:50:22 -0300

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { env } from './config/env';
+import routes from './routes';
+
+import 'reflect-metadata';
+import './database/connect';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(env.PORT, () => console.log(`Server started at http://localhost:${env.PORT}`));
